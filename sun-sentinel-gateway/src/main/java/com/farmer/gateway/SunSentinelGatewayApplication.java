0d4ba36e745ed1cf6c5b664179b78fe14dc1f949
孙治纲 <864307818@qq.com>
2019-09-30 15:58:45 +0800

    第一次提交

---
+++ sun-sentinel-gateway/src/main/java/com/farmer/gateway/SunSentinelGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.farmer.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunSentinelGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunSentinelGatewayApplication.class, args);
+    }
+
+}
